64305b008fb2d713e424eaa2507bafee1cd28ec9
배성환 <baeseongwhan@ulim.co>
2025-07-18 14:59:14 +0900

    테스트 할수 있는 컨트롤러 추가

---
+++ bgm-agit-api/src/main/java/com/bgmagitapi/controller/BgmagitMenuController.java
@@ -0,0 +1,19 @@
+package com.bgmagitapi.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/bgm-agit")
+public class BgmagitMenuController {
+
+    
+    @GetMapping("/menu")
+    public String menu() {
+        return  "test";
+    }
+}
