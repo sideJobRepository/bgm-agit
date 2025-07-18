3b1fd07acb1d195f41ac7a7ce166675609b8e60d
배성환 <baeseongwhan@ulim.co>
2025-07-18 13:08:11 +0900

    CI / CD 프론트 엔드 , 백엔드 테스트

--- bgm-agit-api/src/main/java/com/bgmagitapi/BgmAgitApiApplication.java
+++ bgm-agit-api/src/main/java/com/bgmagitapi/BgmAgitApiApplication.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class BgmAgitApiApplication {
 	
-	
 	public static void main(String[] args) {
 		SpringApplication.run(BgmAgitApiApplication.class, args);
 	}
