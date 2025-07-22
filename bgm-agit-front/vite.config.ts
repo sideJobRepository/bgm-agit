1d16b4164d6e244ad95adefc3f27386614872d32
parkjisu <park74441@naver.com>
2025-07-22 12:21:21 +0900

    원래 변경

--- bgm-agit-front/vite.config.ts
+++ bgm-agit-front/vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
-    base: './',
     server: {
       host: '0.0.0.0',
       port: 5173,
