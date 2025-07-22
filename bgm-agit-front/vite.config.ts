e5e8acb8a19de49ccccf721ea9a72aef86e16b45
PeachCoolpis <rlglxh@naver.com>
2025-07-22 12:05:34 +0900

    롤백

--- bgm-agit-front/vite.config.ts
+++ bgm-agit-front/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
-    base: '/',
+    base: './',
     server: {
       host: '0.0.0.0',
       port: 5173,
