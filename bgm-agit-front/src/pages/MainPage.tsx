9ceb8a0a731402e4d3a219d719a5fb21a1bb7c16
parkjisu <park74441@naver.com>
2025-07-23 19:47:11 +0900

    모바일 갯수수정

--- bgm-agit-front/src/pages/MainPage.tsx
+++ bgm-agit-front/src/pages/MainPage.tsx
@@ -13,9 +13,9 @@ export default function MainPage() {
   const navigate = useNavigate();
 
   const visibleCountMain = isMobile ? 1 : 3;
-  const visibleCountGame = isMobile ? 2 : 3;
+  const visibleCountGame = isMobile ? 3 : 3;
   const visibleCountReserve = isMobile ? 2 : 2;
-  const visibleCountFood = isMobile ? 3 : 4;
+  const visibleCountFood = isMobile ? 4 : 4;
 
   return (
     <Wrapper>
