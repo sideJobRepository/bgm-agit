54dec69e16eaea15139eec6a6729753e216121a9
배성환 <baeseongwhan@ulim.co>
2025-07-30 17:44:10 +0900

    검증 추가

--- bgm-agit-api/src/main/java/com/bgmagitapi/controller/request/BgmAgitRoleModifyRequest.java
+++ bgm-agit-api/src/main/java/com/bgmagitapi/controller/request/BgmAgitRoleModifyRequest.java
@@ -1,15 +1,18 @@
 package com.bgmagitapi.controller.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class BgmAgitRoleModifyRequest {
     
+    @NotNull(message = "멤버 ID를 넣어주세요")
     private Long memberId;
-    
+    @NotNull(message = "권한 ID를 넣어주세요")
     private Long roleId;
 }
