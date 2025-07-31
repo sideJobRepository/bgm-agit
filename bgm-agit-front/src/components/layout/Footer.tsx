864c20c51fc2643e270248ee871dabe43ed5aa11
parkjisu <park74441@naver.com>
2025-07-31 15:36:09 +0900

    푸터 개인정보 추가

--- bgm-agit-front/src/components/layout/Footer.tsx
+++ bgm-agit-front/src/components/layout/Footer.tsx
@@ -12,18 +12,23 @@ export default function Footer() {
   return (
     <Wrapper>
       <Left>
-        <span>찾아오시는 길 : 찾아오시는 길 : 대전 서구 문정로 62 3층 </span>
-        <img
-          src={logo}
-          alt="로고"
-          onClick={() => {
-            kakaoMapGo();
-          }}
-        />
-      </Left>
-      <Right>
+        <div>
+          <span>찾아오시는 길 : 찾아오시는 길 : 대전 서구 문정로 62 3층 </span>
+          <img
+            src={logo}
+            alt="로고"
+            onClick={() => {
+              kakaoMapGo();
+            }}
+          />
+        </div>
         <span>금, 토 : 13:00 ~ 06:00 </span>
         <p>월, 화, 수, 목, 일 : 13:00 ~ 24:00 </p>
+      </Left>
+      <Right>
+        <p> ※ 개인정보 처리방침 </p>
+        <p>BGM아지트는 카카오 로그인 기능을 위해 이메일, 휴대폰, 이름을 수집합니다.</p>
+        <p> 회원 탈퇴 시 모든 정보는 즉시 삭제됩니다.</p>
       </Right>
     </Wrapper>
   );
@@ -32,16 +37,16 @@ export default function Footer() {
 const Wrapper = styled.div<WithTheme>`
   display: flex;
   width: 1500px;
-  padding: 0 20px;
+  padding: 20px;
   min-width: 1023px;
-  height: 100px;
+  height: 100%;
   color: ${({ theme }) => theme.colors.white};
   font-size: ${({ theme }) => theme.sizes.medium};
 
   @media ${({ theme }) => theme.device.tablet} {
     max-width: 100%;
     min-width: 100%;
-    padding: 0 16px;
+    padding: 16px;
     flex-direction: column;
     font-size: ${({ theme }) => theme.sizes.xsmall};
   }
@@ -49,33 +54,48 @@ const Wrapper = styled.div<WithTheme>`
 
 const Left = styled.section<WithTheme>`
   display: flex;
+  flex-direction: column;
   height: 100%;
   width: 50%;
-  align-items: center;
+  align-items: flex-start;
+  justify-content: center;
 
   @media ${({ theme }) => theme.device.tablet} {
     width: 100%;
+    margin-bottom: 20px;
   }
 
-  span {
-    margin-top: 4px;
-  }
+  div {
+    display: flex;
+    span {
+      display: flex;
+      margin-top: 4px;
+    }
 
-  img {
-    width: 30px;
-    margin-left: 12px;
-    cursor: pointer;
+    img {
+      width: 30px;
+      margin-left: 12px;
+      cursor: pointer;
+
+      @media ${({ theme }) => theme.device.tablet} {
+        margin-left: 10px;
+        width: 24px;
+      }
+    }
   }
 `;
 
 const Right = styled.section<WithTheme>`
   margin: auto;
-  text-align: right;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: center;
   width: 50%;
 
   @media ${({ theme }) => theme.device.tablet} {
-    text-align: left;
     width: 100%;
     height: 100%;
+    align-items: center;
   }
 `;
