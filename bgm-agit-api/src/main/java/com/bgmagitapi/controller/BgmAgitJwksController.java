c8ee543dadad5d4e20ca52e2c9e3e0ebde4208e2
배성환 <baeseongwhan@ulim.co>
2025-08-29 14:54:32 +0900

    컨트롤러주석

--- bgm-agit-api/src/main/java/com/bgmagitapi/controller/BgmAgitJwksController.java
+++ bgm-agit-api/src/main/java/com/bgmagitapi/controller/BgmAgitJwksController.java
@@ -1,24 +1,24 @@
-package com.bgmagitapi.controller;
-
-import com.nimbusds.jose.jwk.JWKSet;
-import com.nimbusds.jose.jwk.RSAKey;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
-
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/bgm-agit")
-public class BgmAgitJwksController {
-    
-    private final RSAKey rsaKey;
-    
-    @GetMapping("/.well-known/jwks.json")
-    public Map<String, Object> getJwks() {
-        // 공개키만 포함된 JWK Set 반환
-        return new JWKSet(rsaKey.toPublicJWK()).toJSONObject();
-    }
-}
+//package com.bgmagitapi.controller;
+//
+//import com.nimbusds.jose.jwk.JWKSet;
+//import com.nimbusds.jose.jwk.RSAKey;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.Map;
+//
+//@RestController
+//@RequiredArgsConstructor
+//@RequestMapping("/bgm-agit")
+//public class BgmAgitJwksController {
+//
+//    private final RSAKey rsaKey;
+//
+//    @GetMapping("/.well-known/jwks.json")
+//    public Map<String, Object> getJwks() {
+//        // 공개키만 포함된 JWK Set 반환
+//        return new JWKSet(rsaKey.toPublicJWK()).toJSONObject();
+//    }
+//}
