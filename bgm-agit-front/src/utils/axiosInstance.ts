529dbf216f30bb7ddb8eaabcfb405d2d8722ab0b
배성환 <baeseongwhan@ulim.co>
2025-09-08 17:37:44 +0900

    롤백...

--- bgm-agit-front/src/utils/axiosInstance.ts
+++ bgm-agit-front/src/utils/axiosInstance.ts
@@ -7,7 +7,7 @@ interface AuthAxiosRequestConfig extends InternalAxiosRequestConfig {
 }
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   timeout: 15000,
 });
 
