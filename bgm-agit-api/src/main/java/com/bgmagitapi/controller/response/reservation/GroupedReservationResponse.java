40fbe654710ad03788a47c9fe00ad3d5e0933b09
배성환 <baeseongwhan@ulim.co>
2025-10-13 17:00:53 +0900

    리팩토링

--- bgm-agit-api/src/main/java/com/bgmagitapi/controller/response/reservation/GroupedReservationResponse.java
+++ bgm-agit-api/src/main/java/com/bgmagitapi/controller/response/reservation/GroupedReservationResponse.java
@@ -1,5 +1,6 @@
 package com.bgmagitapi.controller.response.reservation;
 
+import com.bgmagitapi.entity.BgmAgitReservation;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -23,6 +24,34 @@ public class GroupedReservationResponse {
     
     private List<TimeSlot> timeSlots;
     
+    public GroupedReservationResponse(Long reservationNo, List<BgmAgitReservation> list) {
+        this.reservationNo = reservationNo;
+        this.timeSlots = list.stream()
+                .map(r -> new GroupedReservationResponse.TimeSlot(
+                        r.getBgmAgitReservationStartTime().toString(),
+                        r.getBgmAgitReservationEndTime().toString()))
+                .toList();
+        
+        for (BgmAgitReservation reservation : list) {
+            this.reservationDate =  reservation.getBgmAgitReservationStartDate();
+            this.approvalStatus =   reservation.getBgmAgitReservationApprovalStatus();
+            this.cancelStatus =   reservation.getBgmAgitReservationCancelStatus();
+            this.reservationMemberName =  reservation.getBgmAgitMember().getBgmAgitMemberName();
+            this.reservationAddr =  reservation.getBgmAgitImage().getBgmAgitImageLabel();
+            this.reservationPeople = reservation.getBgmAgitReservationPeople();
+            this.reservationRequest =  reservation.getBgmAgitReservationRequest();
+            boolean isMember = reservation.getBgmAgitMember() != null;
+            boolean isPhoneNo = false;
+            if(isMember) {
+                isPhoneNo = reservation.getBgmAgitMember().getBgmAgitMemberPhoneNo() != null;
+            }
+            if (isMember && isPhoneNo) {
+                this.phoneNo = this.replacePhoneNo(reservation.getBgmAgitMember().getBgmAgitMemberPhoneNo());
+            }
+        }
+    
+    }
+    
     @Getter
     @Setter
     public static class TimeSlot {
@@ -33,5 +62,11 @@ public class GroupedReservationResponse {
             this.startTime = startTime;
             this.endTime = endTime;
         }
+    }
+    
+    private String replacePhoneNo(String phoneNo) {
+        return phoneNo
+                .replace("+82", "0")
+                .replaceAll("\\s+", "");
     }
 }
