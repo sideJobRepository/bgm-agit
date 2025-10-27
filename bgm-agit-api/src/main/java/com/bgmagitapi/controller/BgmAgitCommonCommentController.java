7e05118f91158fe31e54f02e0544386fdf970d8e
배성환 <baeseongwhan@ulim.co>
2025-10-27 17:46:49 +0900

    검증 추가

--- bgm-agit-api/src/main/java/com/bgmagitapi/controller/BgmAgitCommonCommentController.java
+++ bgm-agit-api/src/main/java/com/bgmagitapi/controller/BgmAgitCommonCommentController.java
@@ -5,6 +5,9 @@ import com.bgmagitapi.controller.request.BgmAgitCommonCommentPostRequest;
 import com.bgmagitapi.controller.request.BgmAgitCommonCommentPutRequest;
 import com.bgmagitapi.service.BgmAgitCommonCommentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,12 +18,17 @@ public class BgmAgitCommonCommentController {
     private final BgmAgitCommonCommentService bgmAgitCommonCommentService;
     
     @PostMapping("/comment")
-    public ApiResponse createComment(@RequestBody BgmAgitCommonCommentPostRequest request) {
+    public ApiResponse createComment(@Validated @RequestBody BgmAgitCommonCommentPostRequest request, @AuthenticationPrincipal Jwt jwt) {
+        if(jwt == null) {
+            throw new RuntimeException("비 로그인입니다.");
+        }
+        Long memberId = jwt.getClaim("id");
+        request.setMemberId(memberId);
         return bgmAgitCommonCommentService.createComment(request);
     }
     
     @PutMapping("/comment")
-    public ApiResponse modifyComment(@RequestBody BgmAgitCommonCommentPutRequest request) {
+    public ApiResponse modifyComment(@Validated @RequestBody BgmAgitCommonCommentPutRequest request) {
         return bgmAgitCommonCommentService.modifyComment(request);
     }
 }
--- bgm-agit-api/src/main/java/com/bgmagitapi/controller/request/BgmAgitCommonCommentPostRequest.java
+++ bgm-agit-api/src/main/java/com/bgmagitapi/controller/request/BgmAgitCommonCommentPostRequest.java
@@ -1,5 +1,6 @@
 package com.bgmagitapi.controller.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,6 +11,9 @@ import lombok.NoArgsConstructor;
 public class BgmAgitCommonCommentPostRequest {
 
     private Long parentId; // 대댓글이면 있어있어야함
-    private Long freeId;
+    
+    @NotBlank(message = "댓글 내용은 필수 입니다.")
     private String content;
+    
+    private Long memberId;
 }
