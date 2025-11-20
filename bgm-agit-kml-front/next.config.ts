df4a1ef552359a6d071c79406bdd11a2711cd94c
배성환 <baeseongwhan@ulim.co>
2025-11-20 11:32:00 +0900

    설정 수정

--- bgm-agit-kml-front/next.config.ts
+++ bgm-agit-kml-front/next.config.ts
@@ -1,17 +1,23 @@
-// next.config.ts
 
-import type {NextConfig} from "next";
+const nextConfig = {
+  reactStrictMode: true,
 
-const nextConfig: NextConfig = {
-    reactStrictMode: true,
-    async rewrites() {
-        return [
-            {
-                source: "/bgm-agit/:path*",
-                destination: `${process.env.NEXT_PUBLIC_API_URL}/bgm-agit/:path*`,
-            },
-        ];
-    },
+  // /record 로 베이스 경로 지정
+  basePath: '/record',
+
+  async rewrites() {
+    return [
+      {
+        source: '/bgm-agit/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/bgm-agit/:path*`,
+      },
+    ];
+  },
+
+  env: {
+    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
 };
 
 export default nextConfig;
