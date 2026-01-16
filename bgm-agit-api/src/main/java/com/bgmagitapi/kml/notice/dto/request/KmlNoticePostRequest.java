4b7196b1d591a86b73db891f31fbd4fcfdcc729d
배성환 <baeseongwhan@ulim.co>
2026-01-16 15:33:33 +0900

    feat. 검증 추가

--- bgm-agit-api/src/main/java/com/bgmagitapi/kml/notice/dto/request/KmlNoticePostRequest.java
+++ bgm-agit-api/src/main/java/com/bgmagitapi/kml/notice/dto/request/KmlNoticePostRequest.java
@@ -1,5 +1,6 @@
 package com.bgmagitapi.kml.notice.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,7 +16,9 @@ import java.util.List;
 @Builder
 public class KmlNoticePostRequest {
     
+    @NotBlank(message = "제목은 필수입니다.")
     private String title;
+    @NotBlank(message = "내용은 필수입니다..")
     private String cont;
     private List<MultipartFile> files;
     
