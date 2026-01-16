4c576594fa43055f1ebede7a1bed6518dcc678ac
배성환 <baeseongwhan@ulim.co>
2026-01-16 15:33:57 +0900

    feat. 컨트롤러 추가

--- bgm-agit-api/src/main/java/com/bgmagitapi/kml/rule/controller/RuleController.java
+++ bgm-agit-api/src/main/java/com/bgmagitapi/kml/rule/controller/RuleController.java
@@ -1,4 +1,27 @@
 package com.bgmagitapi.kml.rule.controller;
 
+
+import com.bgmagitapi.apiresponse.ApiResponse;
+import com.bgmagitapi.kml.rule.dto.request.RulePostRequest;
+import com.bgmagitapi.kml.rule.entity.Rule;
+import com.bgmagitapi.kml.rule.service.RuleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/bgm-agit")
 public class RuleController {
+
+    
+    private final RuleService ruleService;
+    
+    @PostMapping("/rule")
+    public ApiResponse rule(@RequestBody RulePostRequest request) {
+        return ruleService.createRule(request);
+    }
+    
 }
