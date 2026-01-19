2225ccb76f6a730b35e63312de8fdd961bb4b055
parkjs <park74441@naver.com>
2026-01-19 16:46:21 +0900

    [fix] 디테일 css 수정

--- bgm-agit-kml-front/app/notice/[id]/page.tsx
+++ bgm-agit-kml-front/app/notice/[id]/page.tsx
@@ -206,9 +206,9 @@ const ButtonBox = styled.div`
         position: relative;
         align-items: center;
         justify-content: flex-end;
-        gap: 8px;
-        width: 100%;
+        gap: 4px;
         font-weight: 500;
+        margin-left: 8px;
         color: ${({ theme }) => theme.colors.grayColor};
         font-size: ${({ theme }) => theme.desktop.sizes.sm};
 
@@ -228,7 +228,7 @@ const Button = styled.button<{ color: string }>`
   border: none;
   border-radius: 4px;
   cursor: pointer;
-
+    white-space: nowrap;
   @media ${({ theme }) => theme.device.mobile} {
     font-size: ${({ theme }) => theme.mobile.sizes.md};
   }
