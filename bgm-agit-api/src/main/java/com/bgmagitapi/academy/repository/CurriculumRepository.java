05717b1c56f23fabfbf8db83338a8952b1f75b34
배성환 <baeseongwhan@ulim.co>
2026-02-11 14:24:35 +0900

    feat. 불필요 코드 삭제

--- bgm-agit-api/src/main/java/com/bgmagitapi/academy/repository/CurriculumRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bgmagitapi.academy.repository;
-
-import com.bgmagitapi.academy.entity.Curriculum;
-import com.bgmagitapi.academy.repository.query.CurriculumQueryRepository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CurriculumRepository extends JpaRepository<Curriculum, Long> , CurriculumQueryRepository {
-
-}
