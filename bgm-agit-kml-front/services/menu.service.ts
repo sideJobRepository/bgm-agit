e08ca6f9d803181311baacca8e776d1229cec6ef
parkjisu <park74441@naver.com>
2026-04-07 01:02:16 +0900

    refactor. 로그인, 로그아웃 메뉴 재조회

--- bgm-agit-kml-front/services/menu.service.ts
+++ bgm-agit-kml-front/services/menu.service.ts
@@ -2,12 +2,18 @@ import { useRequest } from '@/hooks/useRequest';
 import { useEffect } from 'react';
 import api from '@/lib/axiosInstance';
 import { useKmlMenuStore } from '@/store/menu';
+import { useUserStore } from '@/store/user';
+import { tokenStore } from '@/services/tokenStore';
 
 export function useFetchMainMenu() {
   const setMainMenu = useKmlMenuStore((state) => state.setMenu);
   const { request } = useRequest();
 
+  const token = tokenStore.get();
+
   useEffect(() => {
-    request(() => api.get('/bgm-agit/kml-menu').then(res => res.data), setMainMenu, {ignoreErrorRedirect: true});
-  }, []);
+    request(() => api.get('/bgm-agit/kml-menu').then((res) => res.data), setMainMenu, {
+      ignoreErrorRedirect: true,
+    });
+  }, [token]);
 }
