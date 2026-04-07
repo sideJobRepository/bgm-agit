e199d497276832bda28b74d1257165e32453a313
배성환 <baeseongwhan@ulim.co>
2026-04-07 09:14:44 +0900

    feat. 셋팅 컨트롤러 추가

--- bgm-agit-api/src/main/java/com/bgmagitapi/kml/setting/controller/SettingController.java
+++ bgm-agit-api/src/main/java/com/bgmagitapi/kml/setting/controller/SettingController.java
@@ -1,9 +1,13 @@
 package com.bgmagitapi.kml.setting.controller;
 
 
+import com.bgmagitapi.apiresponse.ApiResponse;
+import com.bgmagitapi.kml.setting.dto.request.SettingPostRequest;
+import com.bgmagitapi.kml.setting.dto.response.SettingGetResponse;
+import com.bgmagitapi.kml.setting.service.SettingService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/bgm-agit")
@@ -11,6 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class SettingController {
 
 
-
-
+    private final SettingService settingService;
+    
+    
+    @GetMapping("/setting")
+    public SettingGetResponse getSettings() {
+        return settingService.getSettings();
+    }
+    
+    @PostMapping("/setting")
+    public ApiResponse setSettings(@Validated @RequestBody SettingPostRequest request) {
+        return settingService.createSettings(request);
+    }
 }
