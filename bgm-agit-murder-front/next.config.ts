e9c43931672aa303bb3434dc33399563f8f0967b
PeachCoolpis <rlglxh@naver.com>
2026-08-09 19:46:56 +0900

    feat. 머더 프론트 ci cd 추가

--- bgm-agit-murder-front/next.config.ts
+++ bgm-agit-murder-front/next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+
+  basePath: '/murder',
+  assetPrefix: '/murder',
+
+  async rewrites() {
+    return [
+      {
+        source: '/bgm-agit/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/bgm-agit/:path*`,
+      },
+    ];
+  },
+
+  env: {
+    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
 };
 
 export default nextConfig;
